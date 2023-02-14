c513e6fd84d624213af00f2a2b63b2f201fe3438
huyhochanh002 <huyhochanh002@gmail.com>
2023-02-14 14:40:39 +0700

    Huy : Thêm Giao Diện Chính

--- BANGAS_TN/Giaodienchinh.Designer.cs
+++ BANGAS_TN/Giaodienchinh.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Giaodienchinh";
-            this.Text = "Giao DIện Chính";
+            this.Text = "GiaoDienChinh";
             this.ResumeLayout(false);
 
         }
