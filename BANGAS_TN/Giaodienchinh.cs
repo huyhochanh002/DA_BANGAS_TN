26b6771eb5280f4f3afb706756bcb78000fe8669
huyhochanh002 <huyhochanh002@gmail.com>
2023-02-14 14:20:09 +0700

    Huy - Thêm dự án

--- BANGAS_TN/Giaodienchinh.cs
+++ BANGAS_TN/Giaodienchinh.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BANGAS_TN
 {
-    public partial class Form1 : Form
+    public partial class Giaodienchinh : Form
     {
-        public Form1()
+        public Giaodienchinh()
         {
             InitializeComponent();
         }
